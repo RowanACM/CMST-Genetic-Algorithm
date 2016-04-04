1958b799787d766f19ee35d61c6aa0eccbcdfb54
Sean Mulhall <smulhall1337@gmail.com>
2016-04-04 12:25:15 -0400

    completed adding code, does not compile (yet)

--- src/selection.c
+++ src/selection.c
@@ -7,13 +7,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include "individual.h"
+#include "selection.h"
+
+int ind_size = 8;
+
+int main(void)
+{
+	char * pop = malloc(ind_size * 200);
+	for (int i = 0; i < 200; i++)
+	{
+		struct individual * ind = malloc(ind_size);
+		(*ind).fitness = rand() % 10;
+		for (int j = 0; j < ind_size;  j++)
+		{
+		pop[(ind_size * i) + j] = ((char *) ind)[j];
+		}
+		printf("%d ",pop[ind_size *i]);
+		free(ind);
+	}
+
+	char * newPop = selection(pop, 8, 5);
+}
 
 char * selection(char * population, int populationSize, int tournSize)
 {
+
 	char * winners = malloc(ind_size * populationSize);
 	char * bestRef;//pointer to the best individual
 	double best = INT_MAX;//current best fitness
-	double * fitness;
+	double fitness;
 
 	for (int w = 0; w < populationSize; w++)//do populationSize tournaments
 	{
@@ -27,7 +50,10 @@ char * selection(char * population, int populationSize, int tournSize)
 			}
 			for (int j = 0; j < ind_size; j++)//copy the individual into the winners array
 			{
-
+				for (int i = 0; i < ind_size; i++) //copy the individuals byte by byte
+				{
+				winners[(ind_size*j)+i] = ((char*)bestRef)[i];
+				}
 			}
 		}
 	}
