ce1cfcf6d2018dde8c85f9d6623f32d8c5a457ad
Sean Mulhall <smulhall1337@gmail.com>
2016-04-13 14:16:16 -0400

    ensured genome was copying correctly, renamed variables to avoid confusion

--- src/selection.c
+++ src/selection.c
@@ -15,40 +15,49 @@
 char * tournament_selection(char * population, int populationSize, int tournSize)
 {
 
-	char * winners = malloc(ind_size * populationSize);
+	char * winners = malloc(individual_size * populationSize);
 	char * bestRef;//pointer to the best individual
 	double best;
-	double fitness;
+	struct individual * current_fitness;
+	int genome_length = 15;
 	
-	struct individual * ind = malloc(ind_size);
+	struct individual * ind = malloc(individual_size);
 
 	for (int w = 0; w < populationSize; w++)//do populationSize tournaments
 	{
-		best = INT_MAX;//current best fitness(moved down here to reset best every time we set up a new tournament 
+		best = INT_MAX;//current best fitness(moved down here to reset best every time we set up a new tournament) 
 		for (int c = 0; c < tournSize; c++)//pull out tournSize of individuals
 		{
-			fitness = (*(struct individual *)(population + ind_size * (rand()%populationSize))).fitness;//pull out random individual and store its fitness
-			
-			if(fitness < best)//compare fitnesses to the current best(less is better)
+			current_fitness = (struct individual *)(population + individual_size * (rand()%populationSize));//pull out random individual and store its fitness			
+									
+			if((*current_fitness).fitness < best)//compare fitnesses to the current best(less is better)
 			{
-				best = fitness;//assign that fitness to best if its better
-				bestRef = &fitness;//assign the bestRef pointer to that individual
+				best = (*current_fitness).fitness;//assign that fitness to best if its better
+				bestRef = current_fitness;//assign the bestRef pointer to that individual
 			}						
 		}//tourn loop
 		
-		for (int i = 0; i < ind_size; i++) //copy the individuals byte by byte
+		for (int i = 0; i < individual_size; i++) //copy the individuals byte by byte
 		{
-			winners[(ind_size*w)+i] = (bestRef)[i];
+			winners[(individual_size*w)+i] = bestRef[i];
 		}//copy loop
 					
 	}//winners loop 
 	
-	printf("%i\n", populationSize);
+	printf("%i\n", genome_length);
 	
 	
 	for (int i = 0; i < populationSize; i++)
 	{		
-		printf("%f \n", (*(struct individual*)(winners+(ind_size*i))).fitness);			
+		printf("Fitness:%f \n", (*(struct individual*)(winners+(individual_size*i))).fitness);
+		printf("genomes \n");
+		
+		for (int j = 0; j < genome_length; j++)
+		{
+		printf("%i \n", (*(struct individual*)(winners+(individual_size*i))).genome[j]);
+		}
+		
+		printf("\n");		
 	}
 
 	return winners;
