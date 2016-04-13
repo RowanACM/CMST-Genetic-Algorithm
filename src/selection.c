073f26c00acb3300956767a35e879c9428243fb1
Sean Mulhall <smulhall1337@gmail.com>
2016-04-13 13:16:54 -0400

    selection works! will clean up code and refactor things to adhere to master files

--- src/selection.c
+++ src/selection.c
@@ -11,43 +11,20 @@
 #include "individual.h"
 #include "selection.h"
 
-int ind_size = 8;
-/**
-test method
-**/
-/**
-int main(void)
-{
-	char * selection(char * population, int populationSize, int tournSize);
-	char * pop = malloc(ind_size * 200);
-	for (int i = 0; i < 200; i++)
-	{
-		struct individual * ind = malloc(ind_size);
-		(*ind).fitness = rand() % 10;
-		for (int j = 0; j < ind_size;  j++)
-		{
-		pop[(ind_size * i) + j] = ((char *) ind)[j];
-		}
-		printf("%d ",pop[ind_size *i]);
-		free(ind);
-	}
 
-	char * newPop = selection(pop, 8, 5);
-}
-**/
 char * tournament_selection(char * population, int populationSize, int tournSize)
 {
 
 	char * winners = malloc(ind_size * populationSize);
 	char * bestRef;//pointer to the best individual
-	double best;// = INT_MAX;//current best fitness commenting this out for now, because i dont think best will reset to max_int after we enter the loop
+	double best;
 	double fitness;
 	
 	struct individual * ind = malloc(ind_size);
 
 	for (int w = 0; w < populationSize; w++)//do populationSize tournaments
 	{
-		best = INT_MAX;//current best fitness
+		best = INT_MAX;//current best fitness(moved down here to reset best every time we set up a new tournament 
 		for (int c = 0; c < tournSize; c++)//pull out tournSize of individuals
 		{
 			fitness = (*(struct individual *)(population + ind_size * (rand()%populationSize))).fitness;//pull out random individual and store its fitness
@@ -59,27 +36,19 @@ char * tournament_selection(char * population, int populationSize, int tournSize
 			}						
 		}//tourn loop
 		
-		/*for (int j = 0; j < ind_size; j++)//copy the individual into the winners array
-		may take this out, J was originally the position in winners we wanted to store in, but we can 
-		probably just use w, since thats the number of tournaments we've done so far. 
-		...
-		also moved the copy loop down here. The way it was before, we would have been copying bestRef every single time the tourn loop looped
-		*/
-		
-		for (int i = 0; i < ind_size*4; i++) //copy the individuals byte by byte
-		{//might want to think about changing the bound to ind_size*4, since and int is 4 times the size of a char
-		 //and if we cast the winner as a char, we might lose those 3 extra bytes. not sure yet, will talk to nick
-			winners[(ind_size*w)+i] = ((char*)bestRef)[i];
+		for (int i = 0; i < ind_size; i++) //copy the individuals byte by byte
+		{
+			winners[(ind_size*w)+i] = (bestRef)[i];
 		}//copy loop
 					
 	}//winners loop 
 	
+	printf("%i\n", populationSize);
+	
 	
 	for (int i = 0; i < populationSize; i++)
-	{
-		
-		printf("%d \n", winners[i*ind_size]);
-			
+	{		
+		printf("%f \n", (*(struct individual*)(winners+(ind_size*i))).fitness);			
 	}
 
 	return winners;
