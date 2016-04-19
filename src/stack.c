ac86719a59e2642381127d014106d724fec45f88
Heisler <heislerc0@rowan.edu>
2016-04-19 16:28:19 -0400

    Changed implementation of stack.

--- src/stack.c
+++ src/stack.c
@@ -1,23 +1,26 @@
+#include "stack.h"
+
 void push(struct stack s, int number){
 	if(top => s.size){
 		s.size = s.size * 2;
 		realloc(s.stk, s.size);
 	}
-	s.stk[s.top] = number;
 	s.top = s.top + 1;
+	s.stk[s.top] = number;
 }
 
 int pop(struct stack s){
-	if(s.top > 0){
+	if(s.top >= 0)
+		int result = s.stk[s.top];
 		s.top = s.top - 1;
-		return s.stk[top];
+		return result;
 	}
-	return -1;
+	return INT_MAX;
 }
 
 int peak(struct stack s){
 	if(s.top > 0){
-		return s.stk[top - 1];
+		return s.stk[s.top];
 	} 
-	return -1;
+	return INT_MAX;
 }
